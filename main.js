8d96000f8b2f97a162bef762ba5bc53f90307178
L0laapk3 <L0laapk3@gmail.com>
2019-03-02 00:14:39 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function loadFarm() {
+	let width = 10;
+	let height = 10;
+
+
+
+	let farm = document.createElement("farm");
+	let bg = document.createElement("farm-background");
+	farm.appendChild(bg);
+	for (let i = 0; i < width; i++) {
+		let row = document.createElement("farm-background-row");
+		for (let j = 0; j < height; j++) {
+			let tile = document.createElement("farm-background-tile");
+			row.appendChild(tile);
+		}
+		bg.appendChild(row);
+
+	}
+	let farmContainer = document.getElementById("farmContainer");
+	console.log(farmContainer);
+	farmContainer.appendChild(farm);
+}
+
+
+loadFarm();
